961c9a55cb13a81ec44eef4712f3d112a590c23d
Wesley Austin <wjaustin@oakland.edu>
2016-02-04 22:44:27 -0500

    Implimented Ninety_Nine_Bottles_of_Beer() class

--- BeerSong.java
+++ BeerSong.java
@@ -4,7 +4,14 @@ public class BeerSong {
 	}
 	
 	public static void Ninety_Nine_Bottles_of_Beer(){
-		
+		for (int i = 99; i > 2; i--){
+			System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer\n"
+					+ "take one down, pass it around, " + (i-1) + " bottles of beer on the wall.");
+		}
+		System.out.println("2 bottles of beer on the wall, 2 bottles of beer\n"
+				+ "take one down, pass it around, 1 bottle of beer on the wall.");
+		System.out.println("1 bottle of beer on the wall, 1 bottle of beer\n"
+				+ "take one down, pass it around, no more bottles of beer on the wall.");
 	}
 }
 
